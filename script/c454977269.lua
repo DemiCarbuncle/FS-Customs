6d8c13be54d138e1dfd7b9229137afb1074e130d
DemiCarbuncle <112138272+DemiCarbuncle@users.noreply.github.com>
2022-12-12 15:50:52 +0000

    Fix Winda script

--- script/c454977269.lua
+++ script/c454977269.lua
@@ -69,7 +69,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 	function s.spfilter(c,e,tp)
-	return c:IsSetCard(SET_AMAZONESS) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(SET_SHADDOLL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
