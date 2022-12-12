2f3e6e931a86d7d0a6ec152bebf7b97318a4b083
DemiCarbuncle <112138272+DemiCarbuncle@users.noreply.github.com>
2022-12-12 16:02:54 +0000

    Fix accidental war crimes

--- script/c454977269.lua
+++ script/c454977269.lua
@@ -81,6 +81,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
