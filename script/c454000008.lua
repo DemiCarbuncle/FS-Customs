7ce34b45791b962a57ae76a7e4c397268920e0cd
DemiCarbuncle <112138272+DemiCarbuncle@users.noreply.github.com>
2022-12-14 16:50:04 +0000

    Add Sky Striker Aces - Ai

---
+++ script/c454000008.lua
@@ -0,0 +1,128 @@
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x9c),4,2)
+	c:EnableReviveLimit()
+	--Attributes
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(ATTRIBUTE_DARK, ATTRIBUTE_LIGHT, ATTRIBUTE_WIND, ATTRIBUTE_WATER, ATTRIBUTE_FIRE, ATTRIBUTE_EARTH)
+	c:RegisterEffect(e0)
+	--spsummon limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.xyzlimit)
+	c:RegisterEffect(e1)
+	--Unaffected except when targeted
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.immval)
+	c:RegisterEffect(e3)
+	--Cannot target other monsters for attacks
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetValue(s.tgtg)
+	c:RegisterEffect(e3)
+	--Double ATK
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_ATKCHANGE)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(function() return Duel.IsAbleToEnterBP() end)
+	e4:SetCost(s.doublecost)
+	e4:SetOperation(s.doubleop)
+	c:RegisterEffect(e4)
+	--Increase ATK
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_ATKCHANGE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,{id,2})
+	e5:SetCondition(s.atkcon)
+	e5:SetCost(aux.dxmcostgen(1,1,nil))
+	e5:SetTarget(s.atktg)
+	e5:SetOperation(s.atkop)
+	c:RegisterEffect(e5,false,REGISTER_FLAG_DETACH_XMAT)
+end
+--Cannot target other monsters for attacks
+function s.tgtg(e,c)
+	return c~=e:GetHandler()
+end
+--Double ATK
+function s.doublecost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)>500 end
+	Duel.PayLPCost(tp,Duel.GetLP(tp)-500)
+end
+function s.doubleop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.con)
+	e1:SetOperation(s.op)
+	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local tc=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.HintSelection(tc,true)
+		--Double ATK
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(tc:GetAttack()*2)
+		tc:RegisterEffect(e1)
+	end
+end
+--Increase ATK
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local _,d=Duel.GetBattleMonster(tp)
+	return d
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetAttack()>0
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and c:IsRelateToEffect(e) and c:IsFaceup() then
+		--Increase ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(tc:GetAttack())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function s.immval(e,re)
+	if not re:IsActivated() or not e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) or e:GetOwnerPlayer()==re:GetOwnerPlayer() then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return true end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	return not g or not g:IsContains(e:GetHandler())
+end
